60078618a9d30a27ff660f8e29d151d8fcff538d
Mahdi <137503197+Mahdi-Yadi@users.noreply.github.com>
2025-05-31 14:16:53 +0330

    Connecting backend logic to ml.net model🫡

--- FaceRecognition.WPF/Windows/MainWindow.xaml.cs
+++ FaceRecognition.WPF/Windows/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using FaceRecognition_WPF;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 namespace FaceRecognition.WPF;
 public partial class MainWindow : Window
 {
@@ -17,4 +21,45 @@ public partial class MainWindow : Window
     {
         Environment.Exit(0);
     }
+
+    private void btn_selecImage_Click(object sender, RoutedEventArgs e)
+    {
+
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        openFileDialog.Filter = "Image files (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png";
+
+        if(openFileDialog.ShowDialog() == true)
+        {
+            string imagePath = openFileDialog.FileName;
+
+            img_Selected.Source = new BitmapImage(new Uri(imagePath));
+
+            Analyze(imagePath);
+        }
+
+    }
+
+    private void Analyze(string imagePath)
+    {
+        
+        var imageBytes = File.ReadAllBytes(imagePath);
+
+        var input = new MWFaceRecognition.ModelInput()
+        {
+            ImageSource = imageBytes
+        };
+
+        var result = MWFaceRecognition.Predict(input);
+
+        if(result.PredictedLabel == "Man")
+        {
+            lbl_Result.Content = "مرد";
+        }
+        if (result.PredictedLabel == "Woman")
+        {
+            lbl_Result.Content = "زن";
+        }
+    }
+
 }
