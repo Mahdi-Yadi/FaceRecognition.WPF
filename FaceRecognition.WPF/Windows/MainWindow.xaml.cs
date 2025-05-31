cc5efe6a4ceb663e838fbc53642dcc8a6cc0059c
Mahdi <137503197+Mahdi-Yadi@users.noreply.github.com>
2025-05-31 13:58:23 +0330

    User interface design with xaml

---
+++ FaceRecognition.WPF/Windows/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Input;
+namespace FaceRecognition.WPF;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btn_mini_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void btn_Close_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        Environment.Exit(0);
+    }
+}
